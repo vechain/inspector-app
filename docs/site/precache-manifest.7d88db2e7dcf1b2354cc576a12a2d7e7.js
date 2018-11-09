d405e1c14679fe9a9cfc9844be229aa8003f28df
Asbert Ma <stayinalive@msn.cn>
2018-11-09 13:50:22 +0800

    site

---
+++ docs/site/precache-manifest.7d88db2e7dcf1b2354cc576a12a2d7e7.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "b69de69a4ff8ca0abe96ec0b0c180c5b",
+    "url": "/fonts/fa-brands-400.b69de69a.ttf"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "0b5e3a5451fc62d9023ccafc85bc89db",
+    "url": "/fonts/fa-regular-400.0b5e3a54.woff"
+  },
+  {
+    "revision": "25f55e26b9ba2c028589",
+    "url": "/css/chunk-vendors.68141692.css"
+  },
+  {
+    "revision": "42f9fd6acee87559ac0d6a33488db65e",
+    "url": "/img/fa-brands-400.42f9fd6a.svg"
+  },
+  {
+    "revision": "0c41971339b9fc5b1cefb0abad1e2e69",
+    "url": "/img/fa-regular-400.0c419713.svg"
+  },
+  {
+    "revision": "ec0716ae8aa1ba781a1a6bcbce833f6c",
+    "url": "/fonts/fa-brands-400.ec0716ae.eot"
+  },
+  {
+    "revision": "4478b4d7022cad174e4c04246fe622ef",
+    "url": "/img/fa-solid-900.4478b4d7.svg"
+  },
+  {
+    "revision": "8b7a9afd7b95f62e6ee8a72930bfb9ed",
+    "url": "/fonts/fa-brands-400.8b7a9afd.woff"
+  },
+  {
+    "revision": "659c4d58b00226541ef95c3a76e169c5",
+    "url": "/fonts/fa-brands-400.659c4d58.woff2"
+  },
+  {
+    "revision": "df86c0cc08270ea634f2",
+    "url": "/js/app.7814635a.js"
+  },
+  {
+    "revision": "25f55e26b9ba2c028589",
+    "url": "/js/chunk-vendors.0c722952.js"
+  },
+  {
+    "revision": "6493321d567eb0f22bd5112fbcf044a8",
+    "url": "/fonts/fa-regular-400.6493321d.eot"
+  },
+  {
+    "revision": "b48c48ea8457846a5695b139c377d3d1",
+    "url": "/fonts/fa-regular-400.b48c48ea.ttf"
+  },
+  {
+    "revision": "bdadb6ce95c5a2e7b673940721450d3c",
+    "url": "/fonts/fa-regular-400.bdadb6ce.woff2"
+  },
+  {
+    "revision": "fb493903265cad425ccdf8e04fc2de61",
+    "url": "/fonts/fa-solid-900.fb493903.woff2"
+  },
+  {
+    "revision": "bcb927a742a8370b76642fd1a9a749c0",
+    "url": "/fonts/fa-solid-900.bcb927a7.woff"
+  },
+  {
+    "revision": "f29ad0031ad2c1c14b771ce504e2bfa7",
+    "url": "/fonts/fa-solid-900.f29ad003.eot"
+  },
+  {
+    "revision": "48f54f63d7711d0912a9a10205538fc4",
+    "url": "/fonts/fa-solid-900.48f54f63.ttf"
+  },
+  {
+    "revision": "3b6f67dfe1e0cefb4b9531b9b55e9716",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df86c0cc08270ea634f2",
+    "url": "/css/app.066d1e17.css"
+  }
+];
