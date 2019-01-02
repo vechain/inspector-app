fcd54d9a917457d8e6c108ec9f67f386873c3e84
Asbert Ma <stayinalive@msn.cn>
2019-01-02 17:02:20 +0800

    filter changed

--- src/filters.ts
+++ src/filters.ts
@@ -1,5 +1,5 @@
 import {Vue} from 'vue-property-decorator'
 Vue.filter('addr', (v: string) => {
     const temp = v.startsWith('0x') ? v : `0x${v}`
-    return temp.substring(0, 10) + '....' + temp.substring(temp.length - 8, temp.length)
+    return temp.substring(0, 10) + '......' + temp.substring(temp.length - 10, temp.length)
 })
