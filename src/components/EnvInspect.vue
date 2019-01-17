6d746a806b16ee5d11b698b043c4320fbc4bf5b0
Asbert Ma <stayinalive@msn.cn>
2019-01-17 16:51:26 +0800

    vue config

--- src/components/EnvInspect.vue
+++ src/components/EnvInspect.vue
@@ -1,28 +1,34 @@
 <template>
-  <div v-if="isShow">
-    <b-modal :active="show" :canCancel="[]">
-      <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-head-title is-size-4">Connex <strong>not detected</strong></p>
-        </header>
-        <div class="modal-card-body">
-          <div class="is-size-6">It's recommended to open in <strong class="has-text-primary">VeChain Sync</strong>.</div>
-        </div>
-        <footer class="modal-card-foot" style="flex-direction: row-reverse">
-          <p>
-            <a @click="openWithSync" class="button is-small is-primary">Open in</a>or
-            <a :href="syncReleaseUrl">Download</a>  VeChain Sync
-          </p>
-        </footer>
-      </div>
-    </b-modal>
-  </div>
+    <div v-if="isShow">
+        <b-modal :active="show" :canCancel="[]">
+            <div class="modal-card">
+                <header class="modal-card-head">
+                    <p class="modal-card-head-title is-size-4">
+                        Connex
+                        <strong>not detected</strong>
+                    </p>
+                </header>
+                <div class="modal-card-body">
+                    <div class="is-size-6">
+                        It's recommended to open in
+                        <strong class="has-text-primary">VeChain Sync</strong>.
+                    </div>
+                </div>
+                <footer class="modal-card-foot" style="flex-direction: row-reverse">
+                    <p>
+                        <a @click="openWithSync" class="button is-small is-primary">Open in</a>or
+                        <a :href="syncReleaseUrl">Download</a> VeChain Sync
+                    </p>
+                </footer>
+            </div>
+        </b-modal>
+    </div>
 </template>
 <script lang="ts">
-  import { Vue, Component, Prop } from 'vue-property-decorator'
-  const customProtocolDetection = require('custom-protocol-detection')
-  @Component
-  export default class EnvInspect extends Vue {
+import { Vue, Component, Prop } from 'vue-property-decorator'
+// import customProtocolDetection from 'custom-protocol-detection'
+@Component
+export default class EnvInspect extends Vue {
     @Prop(Boolean)
     private isShow!: false
 
@@ -31,10 +37,11 @@
     private syncReleaseUrl = `https://github.com/vechain/thor-sync.electron/releases`
 
     created() {
-      this.show = this.isShow
+        this.show = this.isShow
     }
 
     openWithSync() {
+        const customProtocolDetection = require('custom-protocol-detection')
         const vechainAppUrl = 'vechain-app:///' + encodeURIComponent(window.location.href)
         const gotoDownload = () => {
             window.location.href = this.syncReleaseUrl
@@ -42,11 +49,11 @@
         customProtocolDetection(vechainAppUrl, () => {
             gotoDownload()
         }, () => {
-            console.log('opened with sync')
+            // TODO Open with sync
         }, () => {
             gotoDownload()
         })
     }
-  }
+}
 </script>
 
