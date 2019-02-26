e4b2e64fd4fc0908d1181ff44d8ddb68b8eb5a0e
Asbert Ma <stayinalive@msn.cn>
2019-02-26 10:06:17 +0800

    apply env site

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,35 @@
 <template>
 <body class="has-navbar-fixed-top">
-  <EnvInspect :isShow="!isSync"/>
-  <Navbar v-if="isSync" class="is-fixed-top"/>
-  <router-view v-if="isSync" class="router-view"></router-view>
+    <!-- <EnvInspect :isShow="!isSync"/> -->
+    <Navbar class="is-fixed-top"/>
+    <router-view class="router-view"></router-view>
 </body>
 </template>
 <script lang="ts">
-  import { Vue, Component } from 'vue-property-decorator'
-  import Navbar from './components/Navbar.vue'
-  import EnvInspect from './components/EnvInspect.vue'
+import { Vue, Component } from 'vue-property-decorator'
+import Navbar from './components/Navbar.vue'
+import EnvInspect from './components/EnvInspect.vue'
 
-  @Component({
+@Component({
     components: {
-      Navbar,
-      EnvInspect
+        Navbar,
+        EnvInspect
     }
-  })
-  export default class App extends Vue {
-    private isSync = isSyncEnv
-  }
+})
+export default class App extends Vue {
+}
 </script>
 <style>
-  html {
+html {
     height: 100vh;
-  }
-  body {
+}
+body {
     height: 100%;
-  }
-  .router-view {
+}
+.router-view {
     height: 100%;
     overflow: auto;
-  }
+}
 </style>
 
 
