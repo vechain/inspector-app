8ed2c0e428db6e2bd3171f4c28cb0efa4d43dbe8
Asbert Ma <stayinalive@msn.cn>
2019-03-06 11:24:59 +0800

    layout steady

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,58 +1,58 @@
 <template>
-  <Panel v-model="activeTab" :tabs="tabs" :title="item.name">
-    <template slot="panel-content">
-      <div v-show="activeTab === tabs[0]">
-        <b-field
-          class="item-content"
-          horizontal
-          :label="v.name"
-          v-for="(v, index) in filters"
-          :key="index"
-        >
-          <b-input ref="input" v-model="params[v.name]" :placeholder="v.type"></b-input>
-        </b-field>
-        <b-field class="item-content" horizontal>
-          <div class="buttons has-addons is-pulled-right">
-            <button
-              type="button"
-              @click="addFilter(item.name)"
-              class="button is-rounded is-primary is-outlined"
-            >As a view</button>
-            <button
-              type="button"
-              @click="getResult"
-              class="button is-rounded is-primary is-outlined"
-            >Execute</button>
-            <button
-              v-if="filters.length"
-              type="button"
-              @click="reset"
-              class="button is-rounded is-primary is-outlined"
-            >Reset</button>
-          </div>
-        </b-field>
-      </div>
-      <div v-show="activeTab === tabs[1]">
-        <pre>{{item}}</pre>
-      </div>
-      <div v-show="activeTab === tabs[2]">
-        <LogList :keys="columns" :list="list"/>
-      </div>
-    </template>
-  </Panel>
+    <Panel v-model="activeTab" :tabs="tabs" :title="item.name">
+        <template slot="panel-content">
+            <div v-show="activeTab === tabs[0]">
+                <b-field
+                    class="item-content"
+                    horizontal
+                    :label="v.name"
+                    v-for="(v, index) in filters"
+                    :key="index"
+                >
+                    <b-input ref="input" v-model="params[v.name]" :placeholder="v.type"></b-input>
+                </b-field>
+                <b-field class="item-content" horizontal>
+                    <div class="buttons has-addons">
+                        <button
+                            type="button"
+                            @click="addFilter(item.name)"
+                            class="button is-rounded is-primary is-outlined"
+                        >As a view</button>
+                        <button
+                            type="button"
+                            @click="getResult"
+                            class="button is-rounded is-primary is-outlined"
+                        >Execute</button>
+                        <button
+                            v-if="filters.length"
+                            type="button"
+                            @click="reset"
+                            class="button is-rounded is-primary is-outlined"
+                        >Reset</button>
+                    </div>
+                </b-field>
+            </div>
+            <div v-show="activeTab === tabs[1]">
+                <pre>{{item}}</pre>
+            </div>
+            <div v-show="activeTab === tabs[2]">
+                <LogList :keys="columns" :list="list"/>
+            </div>
+        </template>
+    </Panel>
 </template>
 <script lang="ts">
-  import Panel from './Panel.vue'
-  import LogList from './LogList.vue'
-  import { Vue, Component, Prop } from 'vue-property-decorator'
-  import DB from '../database'
-  @Component({
+import Panel from './Panel.vue'
+import LogList from './LogList.vue'
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import DB from '../database'
+@Component({
     components: {
-      Panel,
-      LogList
+        Panel,
+        LogList
     }
-  })
-  export default class EventCard extends Vue {
+})
+export default class EventCard extends Vue {
     @Prop({ default: null })
     private item: ABI.EventItem | any
 
@@ -68,91 +68,94 @@
     private event!: Connex.Thor.EventVisitor
 
     created() {
-      this.filters.forEach((item: ABI.EventInputItem) => {
-        this.params[item.name] = ''
-      })
-      this.activeTab = this.tabs[0]
-      const account = connex.thor.account(this.address)
-      this.event = account.event(this.item)
+        this.filters.forEach((item: ABI.EventInputItem) => {
+            this.params[item.name] = ''
+        })
+        this.activeTab = this.tabs[0]
+        const account = connex.thor.account(this.address)
+        this.event = account.event(this.item)
     }
 
     get filters() {
-      return this.item.inputs.filter((item: ABI.EventInputItem) => {
-        return item.indexed
-      })
+        return this.item.inputs.filter((item: ABI.EventInputItem) => {
+            return item.indexed
+        })
     }
 
     get columns() {
-      return this.item.inputs.map((item: ABI.EventInputItem) => {
-        return item.name
-      })
+        return this.item.inputs.map((item: ABI.EventInputItem) => {
+            return item.name
+        })
     }
 
     private addFilter(name: string) {
-      this.$dialog.prompt({
-        title: 'Add quick view',
-        message: 'Input a filter name',
-        inputAttrs: {
-          placeholder: 'Filter name',
-          value: name,
-          maxlength: 30,
-          required: true
-        },
-        onConfirm: (value: string) => {
-          this.saveFilter(value)
-        }
-      })
+        this.$dialog.prompt({
+            title: 'Add quick view',
+            message: 'Input a filter name',
+            inputAttrs: {
+                placeholder: 'Filter name',
+                value: name,
+                maxlength: 30,
+                required: true
+            },
+            onConfirm: (value: string) => {
+                this.saveFilter(value)
+            }
+        })
     }
 
     private reset() {
-      this.params = {}
+        this.params = {}
     }
 
     private async saveFilter(name: string) {
-      const contract =
-        (await DB.contracts
-          .where('address')
-          .equals(this.address)
-          .first()) || null
+        const contract =
+            (await DB.contracts
+                .where('address')
+                .equals(this.address)
+                .first()) || null
 
-      await DB.filters.add({
-        name,
-        address: contract!.address,
-        contractName: contract!.name,
-        createdTime: Date.now(),
-        abi: this.item
-      })
-      BUS.$emit('added-filter')
-      this.$toast.open({
-        message: 'Added success!',
-        type: 'is-success'
-      })
+        await DB.filters.add({
+            name,
+            address: contract!.address,
+            contractName: contract!.name,
+            createdTime: Date.now(),
+            abi: this.item
+        })
+        BUS.$emit('added-filter')
+        this.$toast.open({
+            message: 'Added success!',
+            type: 'is-success'
+        })
     }
 
     private async getResult() {
-      const params: any[] = []
+        const params: any[] = []
 
-      for (const key in this.params) {
-        if (this.params.hasOwnProperty(key)) {
-          const element = this.params[key]
-          if (element) {
-            params.push({
-              [key]: element
-            })
-          }
+        for (const key in this.params) {
+            if (this.params.hasOwnProperty(key)) {
+                const element = this.params[key]
+                if (element) {
+                    params.push({
+                        [key]: element
+                    })
+                }
+            }
         }
-      }
 
-      this.list = await this.event
-        .filter(params)
-        .order('desc')
-        .apply(0, 5)
-      this.activeTab = this.tabs[2]
+        this.list = await this.event
+            .filter(params)
+            .order('desc')
+            .apply(0, 5)
+        this.activeTab = this.tabs[2]
     }
-  }
+}
 </script>
 <style lang="scss" scoped>
-  .item-content {
+.item-content {
     padding: 0.5rem 5rem 0 0;
-  }
+}
+.item-content .buttons {
+  justify-content: flex-end;
+}
 </style>
