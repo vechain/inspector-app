14974b046cbdbb2370fb8bafbe42de05fc2ec244
Asbert Ma <stayinalive@msn.cn>
2019-07-22 22:29:09 +0800

    layout

--- src/views/FilterView.vue
+++ src/views/FilterView.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="section">
-    <div class="log-filter">
+    <!-- <div class="log-filter">
       <div class="box">
         <b-field :label="v.name" v-for="(v, index) in params" :key="index">
           <b-input v-model="conditions[v.name]" :placeholder="v.type"></b-input>
@@ -10,9 +10,6 @@
             <option :value="true">ASC</option>
             <option :value="false">DESC</option>
           </b-select>
-        </b-field>
-        <b-field label="More info">
-          <b-switch v-model="metadata"></b-switch>
         </b-field>
         <b-field class="has-text-right">
           <button @click="search" class="button is-primary is-outlined">Search</button>
@@ -58,142 +55,139 @@
           </p>
         </b-field>
       </div>
-    </div>
-    <div class="box log-container">
+    </div> -->
+    <div class="log-container">
       <b-loading :is-full-page="false" :active.sync="isLoading" :can-cancel="true"></b-loading>
-      <LogList v-if="!isLoading" :metadata="metadata" :keys="columns" :list="list"/>
+      <template v-for="(item, i) in list">
+        <EventShowCard :item="item" :key="i" :params="abi.inputs">
+          <span slot="title">#{{i+1}}</span>
+        </EventShowCard>
+      </template>
     </div>
   </section>
 </template>
 <script lang="ts">
-  import LogList from '../components/LogList.vue'
-  import { Vue, Component, Watch } from 'vue-property-decorator'
-  import DB, { Entities } from '../database'
-  @Component({
-    components: {
-      LogList
-    }
-  })
-  export default class FilterView extends Vue {
-    private isLoading = true
-    private filter!: Entities.Filter | null
-    private event!: Connex.Thor.EventVisitor
-    private abi!: any
-    private conditions: any = {}
-    private page = {
-      num: -1,
-      size: 10,
-      order: true
-    }
+import EventShowCard from '../components/EventShowCard.vue'
+import { Vue, Component, Watch } from 'vue-property-decorator'
+import DB, { Entities } from '../database'
+@Component({
+  components: {
+    EventShowCard
+  }
+})
+export default class FilterView extends Vue {
+  private isLoading = true
+  private filter!: Entities.Filter | null
+  private event!: Connex.Thor.EventVisitor
+  private abi!: any
+  private conditions: any = {}
+  private page = {
+    num: -1,
+    size: 10,
+    order: true
+  }
 
-    private list: any[] = []
-    private metadata: boolean = false
-    private columns: string[] = []
-    private params: ABI.EventInputItem[] = []
+  private list: any[] = []
+  private metadata: boolean = false
+  private params: ABI.EventInputItem[] = []
 
-    setParams() {
-      this.params = this.abi.inputs.filter((item: ABI.EventInputItem) => {
-        return item.indexed
-      })
-    }
+  setParams() {
+    this.params = this.abi.inputs.filter((item: ABI.EventInputItem) => {
+      return item.indexed
+    })
+  }
 
-    setColumns() {
-      this.columns = this.abi.inputs.map((item: ABI.EventInputItem) => {
-        return item.name
-      })
-    }
+  @Watch('$route')
+  async onRouterChange() {
+    await this.init()
+    this.nextPage()
+  }
 
-    @Watch('$route')
-    async onRouterChange() {
-      await this.init()
-      this.nextPage()
+  @Watch('conditions')
+  onConditionChange() {
+    this.search()
+  }
+  @Watch('page.size')
+  onSizeChanged(value: number) {
+    if (!value) {
+      this.page.size = 5
     }
+    this.search()
+  }
+  @Watch('page.order')
+  onOrderChanged() {
+    this.search()
+  }
+  get currentPage() {
+    return this.page.num + 1
+  }
 
-    @Watch('conditions')
-    onConditionChange() {
-      this.search()
-    }
-    @Watch('page.size')
-    onSizeChanged(value: number) {
-      if (!value) {
-        this.page.size = 5
-      }
-      this.search()
-    }
-    @Watch('page.order')
-    onOrderChanged() {
-      this.search()
-    }
-    get currentPage() {
-      return this.page.num + 1
-    }
+  private async init() {
+    this.filter =
+      (await DB.filters
+        .where('id')
+        .equals(parseInt(this.$route.params.id, 10))
+        .first()) || null
+    const account = connex.thor.account(this.filter!.address)
+    this.abi = this.filter!.abi ? this.filter!.abi : ''
+    this.event = account.event(this.abi)
+    this.setParams()
+    // this.setColumns()
+  }
 
-    private async init() {
-      this.filter =
-        (await DB.filters
-          .where('id')
-          .equals(parseInt(this.$route.params.id, 10))
-          .first()) || null
-      const account = connex.thor.account(this.filter!.address)
-      this.abi = this.filter!.abi ? this.filter!.abi : ''
-      this.event = account.event(this.abi)
-      this.setParams()
-      this.setColumns()
-    }
+  private search() {
+    this.page.num = -1
+    this.nextPage()
+  }
 
-    private search() {
-      this.page.num = -1
-      this.nextPage()
-    }
+  private prePage() {
+    --this.page.num
+    this.getList()
+  }
+  private nextPage() {
+    ++this.page.num
+    this.getList()
+  }
 
-    private prePage() {
-      --this.page.num
-      this.getList()
-    }
-    private nextPage() {
-      ++this.page.num
-      this.getList()
-    }
+  private async getList() {
+    this.isLoading = true
+    const params: any[] = []
 
-    private async getList() {
-      this.isLoading = true
-      const params: any[] = []
-
-      for (const key in this.conditions) {
-        if (this.conditions.hasOwnProperty(key)) {
-          const element = this.conditions[key]
-          if (element) {
-            params.push({
-              [key]: element
-            })
-          }
+    for (const key in this.conditions) {
+      if (this.conditions.hasOwnProperty(key)) {
+        const element = this.conditions[key]
+        if (element) {
+          params.push({
+            [key]: element
+          })
         }
       }
-
-      this.list = await this.event
-        .filter(params)
-        .order(this.page.order ? 'asc' : 'desc')
-        .apply(this.page.num * this.page.size, this.page.size)
-      this.isLoading = false
     }
 
-    private async created() {
-      await this.init()
-      this.nextPage()
-    }
+    this.list = await this.event
+      .filter(params)
+      .order(this.page.order ? 'asc' : 'desc')
+      .apply(this.page.num * this.page.size, this.page.size)
+    this.isLoading = false
   }
+
+  private async created() {
+    await this.init()
+    this.nextPage()
+  }
+}
 </script>
 <style scoped>
-  .log-filter {
-    width: 250px;
-    right: 20px;
-    float: right;
-  }
-  .log-container {
-    width: calc(100% - 270px);
-    overflow-x: auto;
-    margin-right: 20px;
-    float: left;
-    min-height: 100%;
-  }
+.log-filter {
+  width: 250px;
+  right: 20px;
+  float: right;
+}
+.log-container {
+  width: 800px;
+  margin: auto;
+  padding: 1.1rem;
+  overflow-x: auto;
+  min-height: 100%;
+}
 </style>
