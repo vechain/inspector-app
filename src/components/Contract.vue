42c22c7fd2a6378d7812fed1c65c92c2f769e15e
Asbert Ma <stayinalive@msn.cn>
2019-07-24 13:58:48 +0800

    font family

--- src/components/Contract.vue
+++ src/components/Contract.vue
@@ -1,60 +1,69 @@
 <template>
-  <div class="box" :class="{'img-hover': $listeners.select}">
-    <article class="media">
-      <div class="media-left">
-        <figure :class="{'could-hover': $listeners.select}" class="image is-64x64">
-          <img @click.stop="$emit('select')" class="is-rounded" v-ident="item.address" alt="Image">
-        </figure>
-      </div>
-      <div class="media-content">
-        <div class="content">
-          <p>
-            <strong>{{item.name || ''}}</strong>
-          </p>
-          <p class="is-fixed-font is-size-7" v-if="isShort">{{item.address | addr}}</p>
-          <p class="is-fixed-font is-size-6" v-else>{{item.address}}</p>
-        </div>
-        <nav class="level">
-          <div class="level-left">
-            <div class="level-item">
-              <slot/>
-              <!-- <a class="button is-info">Submit</a> -->
+    <div class="box" :class="{'img-hover': $listeners.select}">
+        <article class="media">
+            <div class="media-left">
+                <figure :class="{'could-hover': $listeners.select}" class="image is-64x64">
+                    <img
+                        @click.stop="$emit('select')"
+                        class="is-rounded"
+                        v-ident="item.address"
+                        alt="Image"
+                    />
+                </figure>
             </div>
-          </div>
-        </nav>
-      </div>
-      <div class="media-right">
-        <div class="content">
-          <slot name="right"/>
-        </div>
-      </div>
-    </article>
-  </div>
+            <div class="media-content">
+                <div class="content">
+                    <p>
+                        <strong>{{item.name || ''}}</strong>
+                    </p>
+                    <p class="is-family-monospace has-text-weight-semibold display-6"
+                        v-if="isShort"
+                    >{{item.address | addr}}</p>
+                    <p class="is-family-monospace has-text-weight-semibold display-6" v-else>
+                        <a target="_blank" :href="`https://insight.vecha.in/#/accounts/${item.address}`">{{item.address}}</a>
+                    </p>
+                </div>
+                <nav class="level">
+                    <div class="level-left">
+                        <div class="level-item">
+                            <slot />
+                            <!-- <a class="button is-info">Submit</a> -->
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <div class="media-right">
+                <div class="content">
+                    <slot name="right"/>
+                </div>
+            </div>
+        </article>
+    </div>
 </template>
 <script lang="ts">
-  import { Vue, Component, Prop } from 'vue-property-decorator'
+import { Vue, Component, Prop } from 'vue-property-decorator'
 
-  @Component
-  export default class Contract extends Vue {
+@Component
+export default class Contract extends Vue {
     @Prop()
     private item!: Contract.Item
 
     @Prop({ default: true })
     private isShort!: boolean
-  }
+}
 </script>
 <style lang="css" scoped>
-  .box {
+.box {
     margin: auto;
-  }
+}
 
-  .img-hover .could-hover {
+.img-hover .could-hover {
     cursor: pointer;
     transition: transform 0.2s, filter 0.2s ease-in-out;
-  }
-  .img-hover:hover .could-hover {
+}
+.img-hover:hover .could-hover {
     transform: scale(1.1);
     filter: brightness(1.2);
-  }
+}
 </style>
 
