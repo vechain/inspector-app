45a6b78c2ee349f6408ffaeae5bb94b10b217876
Asbert Ma <stayinalive@msn.cn>
2019-07-24 13:59:14 +0800

    add checksum address filter

--- src/filters.ts
+++ src/filters.ts
@@ -1,9 +1,14 @@
-import {Vue} from 'vue-property-decorator'
+import { Vue } from 'vue-property-decorator'
+import { toChecksumAddress } from 'thor-devkit/es6/cry/address'
 Vue.filter('addr', (v: string) => {
     const temp = v.startsWith('0x') ? v : `0x${v}`
-    return temp.substring(0, 10) + '......' + temp.substring(temp.length - 10, temp.length)
+    return temp.substring(0, 8) + '...' + temp.substring(temp.length - 8, temp.length)
 })
 
 Vue.filter('datetime', (v: number) => {
     return new Date(v).toLocaleString()
+})
+
+Vue.filter('toChecksumAddress', (val: string) => {
+    return toChecksumAddress(val)
 })
