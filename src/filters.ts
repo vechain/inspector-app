edb451c9147d727ec3dc224aa79f3424d96aedee
Asbert Ma <stayinalive@msn.cn>
2019-07-31 17:31:25 +0800

    replace "..."

--- src/filters.ts
+++ src/filters.ts
@@ -2,7 +2,7 @@ import { Vue } from 'vue-property-decorator'
 import { toChecksumAddress } from 'thor-devkit/es6/cry/address'
 Vue.filter('addr', (v: string) => {
     const temp = v.startsWith('0x') ? v : `0x${v}`
-    return temp.substring(0, 8) + '...' + temp.substring(temp.length - 8, temp.length)
+    return temp.substring(0, 8) + '…' + temp.substring(temp.length - 8, temp.length)
 })
 
 Vue.filter('datetime', (v: number) => {
