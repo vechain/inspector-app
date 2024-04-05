5d54387f41d149a79db1f1fcb7f82cb0ef0724d7
Darren Kelly <107671032+darrenvechain@users.noreply.github.com>
2024-04-05 10:29:34 +0100

    fix: updating vue to latest (#128)

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,20 @@
-module.exports = {
+const { defineConfig } = require("@vue/cli-service");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+module.exports = defineConfig({
   publicPath: '/',
   pwa: {
     iconPaths: {
       favicon32: 'img/icons/favicon-128.ico',
       favicon16: 'img/icons/favicon-64.ico'
     }
-  }
-}
+  },
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [new NodePolyfillPlugin()],
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+      },
+    },
+  },
+});
